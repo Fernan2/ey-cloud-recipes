4ffee0de4733fb585329bd846ccc7b648c089982
Fernan2 <fernandorankia@gmail.com>
2011-09-16 09:49:44 +0200

    Revert 6018209384260ac7aa44ee5531881c204656837d^..HEAD

--- cookbooks/eybackup_slave/recipes/default.rb
+++ cookbooks/eybackup_slave/recipes/default.rb
@@ -1,6 +1,6 @@
 # set backup interval
 cron_hour = if node[:backup_interval].to_s == '24'
-              "1"    # 0100 Pacific, per support's request
+              "19"    # 0100 Pacific, per support's request -> 19 -> 4am en Madrid
               # NB: Instances run in the Pacific (Los Angeles) timezone
             elsif node[:backup_interval]
               "*/#{node[:backup_interval]}"
@@ -8,14 +8,7 @@ cron_hour = if node[:backup_interval].to_s == '24'
               "1"
             end
 
-
 if ['db_master'].include?(node[:instance_role])
-  cron "mysql" do
-    action :delete
-  end
-end
-
-if ['db_slave'].include?(node[:instance_role])
   cron "mysql" do
     minute   '10'
     hour     cron_hour
@@ -24,5 +17,12 @@ if ['db_slave'].include?(node[:instance_role])
     weekday  '*'
     command  "/usr/local/ey_resin/bin/eybackup"
     not_if { node[:backup_window].to_s == '0' }
-  end  
+  end
 end
+
+if ['db_slave'].include?(node[:instance_role])
+  cron "mysql" do
+    action :delete
+  end
+end
+
