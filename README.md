54d99656dfc99472d5ebda0fc73923bb8491c47e
Fernan2 <fernandorankia@gmail.com>
2011-10-18 10:35:42 +0200

    El que sí que va... poniéndolo "a piñón", ya que no sabemos el nombre del node[:instance_role]

--- README.md
+++ README.md
@@ -9,8 +9,15 @@ Follow these steps to use custom deployment recipes with your applications.
 
 * Install the engineyard gem:
   $ gem install engineyard
+# Ejecutado "gem install engineyard" en local
+
 * Add any custom recipes or tweaks to your copy of these recipes.
+# Modificamos eybackup_slave
+
 * Upload them with: `ey recipes upload -e ENV`, where ENV is the name of your environment in Engine Yard Cloud. This may be different than your Rails environment name.
+# cd ey-cloud-recipes
+# ey recipes upload -e verema001
+
 * Once you have completed these steps, each rebuild will run the your
   recipes after the default Engine Yard recipes have run. When you
   update your recipes, just re-run `ey recipes upload -e ENV`.
--- cookbooks/eybackup_slave/recipes/default.rb
+++ cookbooks/eybackup_slave/recipes/default.rb
@@ -1,28 +1,28 @@
 # set backup interval
-cron_hour = if node[:backup_interval].to_s == '24'
-              "19"    # 0100 Pacific, per support's request -> 19 -> 4am en Madrid
-              # NB: Instances run in the Pacific (Los Angeles) timezone
-            elsif node[:backup_interval]
-              "*/#{node[:backup_interval]}"
-            else
-              "1"
-            end
+# cron_hour = if node[:backup_interval].to_s == '24'
+#               "19"    # 0100 Pacific, per support's request -> 19 -> 4am en Madrid
+#               # NB: Instances run in the Pacific (Los Angeles) timezone
+#             elsif node[:backup_interval]
+#               "*/#{node[:backup_interval]}"
+#             else
+#               "1"
+#             end
 
-if ['db_master'].include?(node[:instance_role])
+# if ['db_master'].include?(node[:instance_role])
   cron "mysql" do
     minute   '10'
-    hour     cron_hour
+    hour     '19'
     day      '*'
     month    '*'
     weekday  '*'
     command  "/usr/local/ey_resin/bin/eybackup"
     not_if { node[:backup_window].to_s == '0' }
   end
-end
-
-if ['db_slave'].include?(node[:instance_role])
-  cron "mysql" do
-    action :delete
-  end
-end
-
+# end
+#
+# if ['db_slave'].include?(node[:instance_role])
+#   cron "mysql" do
+#     action :delete
+#   end
+# end
+#
